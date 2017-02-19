5d6ae03a88651d52af406439277a3ff28d0ba833
Qiao Lin <qiaoclin@gmail.com>
2017-02-19 21:24:16 +0100

    Initial Commit

---
+++ DynamicField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicField
+//
+//  Created by Qiao Lin on 2/19/17.
+//  Copyright © 2017 Qiao Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
